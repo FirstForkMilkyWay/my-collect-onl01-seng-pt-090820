5e1cb6abe759e7752f3297c4b6f619653927e223
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-16 12:33:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(collection)
   i = 0
   collection = []
   while i < collection.length
-    collection << yield(array[i])
+    collection << yield(collection[i])
     i += 1
   end
   collection

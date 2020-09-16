375fae1747160f841ffe6dc980f556bcdafbb31e
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-16 12:14:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,6 +9,6 @@ def my_collect(collection)
 end
 
 collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
+my_collect(collection) do |student|
+  student.split(" ").first
 end
